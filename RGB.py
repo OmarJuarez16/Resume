1139ccca143bd3d9cedae0c0a5dc75af8f1aa8d6
OmarJuarez16 <71157455+OmarJuarez16@users.noreply.github.com>
2020-09-13 16:55:25 -0500

    Create RGB.py

---
+++ RGB.py
@@ -0,0 +1,12 @@
+"""
+Date: August/12/2020
+Code - RGB Color Space
+author: OmarJuarez16
+@email: omarandre97@gmail.com
+  
+  Objective: This code analyzes the behavior of the RGB space against adversarial attacks. 
+  
+  References: 
+  - Towards Deep Learning Models Resistant To Adversarial Attack — Madry et al 2017
+  
+"""
